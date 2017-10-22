6dd9d67f62e45134c1a29e311e3d5e2d42979c0f
Matt Borgman <mgborgman@gmail.com>
2017-10-22 08:33:27 -0700

    ability to sort by completed added

--- todo.rb
+++ todo.rb
@@ -8,6 +8,57 @@ configure do
   set :session_secret, 'secret'
 end
 
+helpers do
+  def all_completed?(list)
+    result = ''
+    todos = []
+    if list[:todos].all? {|todo| todo[:completed] == true}
+      return 'class="complete"'
+    end
+  end
+
+  def get_number_of_todos_completed(list)
+    count = 0
+    return 0 if list.empty?
+    list.each do |todo|
+      if todo[:completed] == true
+        count += 1
+      end
+    end
+    count
+  end
+
+  def sort_lists_by_completed(lists)
+    completed = {}
+    not_completed = {}
+    lists.each_with_index do |list, index|
+      if all_completed?(list)
+        completed[index] = list
+      else
+        not_completed[index] = list
+      end
+    end
+    not_completed.merge(completed)
+  end
+
+  def sort_list_by_completed(list)
+    completed = {}
+    not_completed = {}
+    list[:todos].each_with_index do |todo, index|
+      if is_completed?(todo)
+        completed[index] = todo 
+      else
+        not_completed[index] = todo
+      end
+    end
+    not_completed.merge(completed)
+  end
+
+  def is_completed?(todo)
+    todo[:completed] == true
+  end
+end
+
 before do 
   session[:lists] ||= []
 end
@@ -51,18 +102,21 @@ post "/lists" do
   end
 end
 
+# show a specific list
 get "/lists/:list_id" do
   @id = params[:list_id].to_i
   @list = session[:lists][@id]
   erb :list, layout: :layout
 end
 
+# form to edit a specific list
 get "/lists/:list_id/edit" do
   @id = params[:list_id].to_i
   @list = session[:lists][@id]
   erb :edit_list, layout: :layout
 end
 
+# submit form to edit specifc list
 post "/lists/:list_id" do
   list_name = params[:list_name].strip
   @id = params[:list_id].to_i
@@ -79,9 +133,61 @@ post "/lists/:list_id" do
   end
 end
 
+# error handling for todo names
+def error_for_todo_name(name)
+  if !(1..100).cover?(name.size)
+    "Todo name must be between 1 and 100 characters."
+  elsif @list[:todos].any?{|todo| todo == name}
+    "Todo name must be unique."
+  end  
+end
+
+# deleting a list
 post "/lists/:list_id/destroy" do
   @id = params[:list_id].to_i
   session[:lists].delete_at(@id)
   session[:success] = "The list has been deleted."
   redirect "/lists"
+end
+
+# submiting a new todo to a list
+post "/lists/:list_id/todos" do
+  @id = params[:list_id].to_i
+  @list = session[:lists][@id]
+  todo_name = params[:todo].strip 
+  error = error_for_todo_name(todo_name)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    session[:success] = "The todo has been created."
+    @list[:todos] << {name: todo_name, completed: false}
+    redirect "/lists/#{@id}"
+  end
+end
+
+post "/lists/:list_id/todos/:todo_id/destroy" do
+  @id = params[:list_id].to_i
+  @list = session[:lists][@id]
+  @list[:todos].delete_at(params[:todo_id].to_i)
+  session[:success] = "The todo has been deleted."
+  redirect "/lists/#{@id}"
+end
+
+post "/lists/:list_id/todos/:todo_id" do
+  @id = params[:list_id].to_i
+  @list = session[:lists][@id]
+  todo_id = params[:todo_id].to_i
+  is_completed = params[:completed] == 'true'
+  @list[:todos][todo_id][:completed] = is_completed
+  session[:success] = "The todo has been updated."
+  redirect "/lists/#{@id}"
+end
+
+post "/lists/:list_id/complete_all" do
+  @id = params[:list_id].to_i
+  @list = session[:lists][@id]
+  @list[:todos].each{|todo| todo[:completed] = true}
+  session[:success] = "All todos have been completed."
+  redirect "/lists/#{@id}"
 end
